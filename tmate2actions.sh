8f73858d668b8ce2ec7f60e1f394982a56399742
Chenyang Gao <gps949@outlook.com>
2021-02-02 12:18:24 +0800

    Update tmate2actions.sh

--- tmate2actions.sh
+++ tmate2actions.sh
@@ -6,17 +6,16 @@ START_TIME=`date +%s`
 # install the zerotier
 git clone https://github.com/gps949/UpZTProxy.git
 
-sudo cp ./UpZTProxy/zerotier-one ./zerotier-one
-sudo ./zerotier-one -d
+sudo ./UpZTProxy/zerotier-one -d
 
 set -e
-ZEROTIER_NODEID=`sudo ./zerotier-one -q info | cut -d ' ' -f 3`
+ZEROTIER_NODEID=`sudo ./UpZTProxy/zerotier-one -q info | cut -d ' ' -f 3`
 ZEROTIER_LOG="/tmp/zerotier_add_member.log"
 ZEROTIER_CTRLID=${ZEROTIER_NETWORK_ID:0:10}
 
-sudo ./zerotier-one -q join ${ZEROTIER_NETWORK_ID}
-sudo ./zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowGlobal=true
-sudo ./zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowDefault=1
+sudo ./UpZTProxy/zerotier-one -q join ${ZEROTIER_NETWORK_ID}
+sudo ./UpZTProxy/zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowGlobal=true
+sudo ./UpZTProxy/zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowDefault=1
 #sudo zerotier-one -q orbit ${ZEROTIER_MOON_ID} ${ZEROTIER_MOON_ID}
 
 set -e
