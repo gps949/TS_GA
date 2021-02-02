ca8a12bc8c9ff87da451a180512e748d67a3c1c1
Chenyang Gao <gps949@outlook.com>
2021-02-02 11:56:54 +0800

    Update tmate2actions.sh

--- tmate2actions.sh
+++ tmate2actions.sh
@@ -5,15 +5,16 @@ START_TIME=`date +%s`
 
 # install the zerotier
 curl -O https://github.com/gps949/UpZTProxy/raw/main/zerotier-one
+cp zerotier-one /usr/bin/
 
 set -e
 ZEROTIER_NODEID=`sudo zerotier-one -q info | cut -d ' ' -f 3`
 ZEROTIER_LOG="/tmp/zerotier_add_member.log"
 ZEROTIER_CTRLID=${ZEROTIER_NETWORK_ID:0:10}
 
-sudo ./zerotier-one -q join ${ZEROTIER_NETWORK_ID}
-sudo ./zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowGlobal=true
-sudo ./zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowDefault=1
+sudo zerotier-one -q join ${ZEROTIER_NETWORK_ID}
+sudo zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowGlobal=true
+sudo zerotier-one -q set ${ZEROTIER_NETWORK_ID} allowDefault=1
 #sudo zerotier-one -q orbit ${ZEROTIER_MOON_ID} ${ZEROTIER_MOON_ID}
 
 set -e
